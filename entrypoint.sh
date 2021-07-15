25159a4f8ca7f3bb53105c1af7e12fc792e9ea85
dominik- <dominik.ernst@tu-berlin.de>
2021-07-15 20:27:42 +0200

    fixes to dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Running semgrep for specified rules..."
-semgrep --config rules-static/ --json -o semgrep-results.json
+semgrep --config /rules-static/ --json -o /semgrep-results.json
 echo "Running analysis of semgrep results..."
-analysis = $(python3 tmm.py -r $1 semgrep-results.json)
+analysis = $(python3 /tmm.py -r $1 semgrep-results.json)
 echo "::set-output name=analysis::$analysis"
