9545d49d57178089ae81a3436280c4342065c2f2
dominik- <dominik.ernst@tu-berlin.de>
2021-07-15 21:07:50 +0200

    more fixes to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "Running semgrep for specified rules..."
-semgrep --config /rules-static/ --json -o semgrep-results.json ${GITHUB_WORKSPACE}
+semgrep --config /rules-static/ --json -o ${GITHUB_WORKSPACE}/semgrep-results.json ${GITHUB_WORKSPACE}
 echo "Running analysis of semgrep results..."
-analysis = $(python3 /tmm.py -r $1 semgrep-results.json)
+analysis = $(python3 /tmm.py -r $1 ${GITHUB_WORKSPACE}/semgrep-results.json)
 echo "::set-output name=analysis::$analysis"
