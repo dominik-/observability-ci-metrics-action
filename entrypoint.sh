0e8d37969a2ad09b957e46fa190132ef66cece38
dominik- <dominik.ernst@tu-berlin.de>
2021-07-15 22:42:33 +0200

    more fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "Running semgrep for specified rules..."
 semgrep --config /rules-static/ --json -o /semgrep-results.json ${GITHUB_WORKSPACE}
 echo "Running analysis of semgrep results..."
-analysis = $(python3 /tmm.py -r $1 /semgrep-results.json)
+analysis=$(python3 /tmm.py -r $1 /semgrep-results.json)
 echo "::set-output name=analysis::$analysis"
